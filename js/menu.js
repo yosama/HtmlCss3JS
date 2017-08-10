9204f2b49efacd2a842012095cb92ccf0c37df77
Yosnier Samon M <yosamac@gmail.com>
2017-08-10 13:20:11 +0200

    finished scroll spy

--- js/menu.js
+++ js/menu.js
@@ -51,3 +51,53 @@ function deleteClassActive (){
         navbarItems[i].classList.remove('active');
     }
 }
+
+var accumulativeOffset = function (element) {
+    var top = 0;
+    do {
+        top += element.offsetTop || 0;
+        element = element.offsetParent;
+    } while (element)
+    return top;
+}
+
+var offsetAboutMe = accumulativeOffset(document.getElementById("about-me")) - 50;
+var offsetTeam = accumulativeOffset(document.getElementById("team")) - 50;
+var offsetTransport = accumulativeOffset(document.getElementById("transport")) - 50;
+var navbar = document.getElementsByClassName("navbar")[0];
+
+window.addEventListener('scroll', changeMenuStyle);
+
+var previous;
+function changeMenuStyle (){
+    var pageOffset = window.pageYOffset;
+
+    if (pageOffset >= 0 && pageOffset < offsetAboutMe) {
+        if (!previous || previous !== 1 ){
+            previous = 1;
+        } else if (previous === 1){
+            return false;
+        }
+        deleteClassActive()
+        document.querySelector('a[href="#"]').parentNode.classList.add('active');
+        console.log("misma seccion");
+    } else if (pageOffset >= offsetAboutMe && pageOffset < offsetTeam) {
+        if (!previous || previous !== 2){
+            previous = 2;
+        } else if (previous === 2){
+            return false;
+        }
+        deleteClassActive()
+        document.querySelector('a[href$="about-me"]').parentNode.classList.add('active');
+        console.log("misma seccion");
+    } else if (pageOffset >= offsetTeam && offsetAboutMe < offsetTransport) {
+        if (!previous || previous !== 3 ){
+            previous = 3;
+        } else if (previous === 3){
+            return false;
+        }
+        deleteClassActive()
+        document.querySelector('a[href$="team"]').parentNode.classList.add('active');
+        console.log("misma seccion");
+    }
+}
